704d100ddf6783dbf1ad05e58767668ef4c164c2
DITSOB <melonbread2248@gmail.com>
2025-02-07 07:43:55 +0545

    revised cards

--- resources/views/recipes.blade.php
+++ resources/views/recipes.blade.php
@@ -7,12 +7,12 @@
     <h1 class="heading">Recipes</h1>
     <div class="box-container">
         @foreach($recipes as $recipe)
-            <div class="box">
-                <h3>{{ $recipe['title'] }}</h3>
+            <div class="box1">
+                <a href="" style="text-decoration: none;"><h3>{{ $recipe['title'] }}</h3></a>
                 <img src="{{ $recipe['image_url'] }}" alt="{{ $recipe['title'] }}" style="width: 300px; height: 400px; object-fit: cover; border-radius: 10px;">
-                <p class="card-text"><small class="text-muted">{{ $recipe['servings'] }}</small></p>
-                <p class="card-text"><small class="text-muted"><h4 style="font-weight: bold; color: black;">Ingredients</h4><br>{!! nl2br(e($recipe['ingredients'])) !!}</small></p>
-                <p class="card-text"><small class="text-muted"><h4>Instructions</h4><br>{!! nl2br(e($recipe['instructions'])) !!}</small></p>
+                <p class="card-text"><small class="text-muted"><i class="bi bi-person"></i>{{ $recipe['servings'] }}</small></p>
+                <!-- <p class="card-text"><small class="text-muted"><h4 style="font-weight: bold; color: black;">Ingredients</h4><br>{!! nl2br(e($recipe['ingredients'])) !!}</small></p>
+                <p class="card-text"><small class="text-muted"><h4>Instructions</h4><br>{!! nl2br(e($recipe['instructions'])) !!}</small></p> -->
             </div>
         @endforeach
     </div>
--- resources/views/search-recipes.blade.php
+++ resources/views/search-recipes.blade.php
@@ -4,14 +4,18 @@
 <div class="recipe-container">
     
 @if(!empty($recipes))
-    <h1 class="heading">{{ Session::get('search')}}</h1>
+        
+    <h1 class="heading">Search: {{ Session::get('search')}}</h1>
+
     <div class="box-container">
         @foreach($recipes as $recipe)
-            <div class="box">
-                <h3>{{ $recipe['title'] }}</h3>
+            <div class="box1">
+                <a href="" style="text-decoration: none;"><h3>{{ $recipe['title'] }}</h3></a>
                 <img src="{{ $recipe['image_url'] }}" alt="{{ $recipe['title'] }}" style="width: 300px; height: 400px; object-fit: cover; border-radius: 10px;">
                 <p class="card-text"><small class="text-muted"><i class="bi bi-person"></i>{{ $recipe['servings'] }}</small></p>
-                <a href="#" class="btn btn-success" style="color: white; text-decoration: none;">Show Recipe</a>
+                <a href="#" class="btn btn-success" style="color: white; text-decoration: none;">View Recipe</a>
+                <!-- <p class="card-text"><small class="text-muted"><h4 style="font-weight: bold; color: black;">Ingredients</h4><br>{!! nl2br(e($recipe['ingredients'])) !!}</small></p>
+                <p class="card-text"><small class="text-muted"><h4>Instructions</h4><br>{!! nl2br(e($recipe['instructions'])) !!}</small></p> -->
             </div>
         @endforeach
     </div>
